fc794da3d08d84607b13925042e2abdb238d4241
Charles Adu Boakye <4cyberlord@proton.me>
2023-07-30 13:01:24 +0200

    Role Table Implemented with the create functionality enabled to create new Roles within the application

---
+++ resources/views/admin/roles/create.blade.php
@@ -0,0 +1,30 @@
+<x-admin-layout>
+    <form method="POST" action={{ route('admin.role.store') }}
+        class="space-y-8 divide-y divide-gray-200 py-20 w-full px-8">
+        @csrf
+        <div class="space-y-8 divide-y divide-gray-200">
+            <div class="pt-8">
+                <div>
+                    <h3 class="text-lg leading-6 font-medium text-gray-900">Create a Role</h3>
+                </div>
+                <div class="mt-6 grid grid-cols-1 gap-y-6 gap-x-6 sm:grid-cols-6">
+                    <div class="sm:col-span-4 justify-center">
+                        <label for="name" class="block text-sm font-medium text-gray-700"> Role Name </label>
+                        <div class="mt-1">
+                            <input id="name" name="name" type="text"
+                                class="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="pt-5">
+            <div class="flex justify-end">
+                <button type="button"
+                    class="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Cancel</button>
+                <button type="submit"
+                    class="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Save</button>
+            </div>
+        </div>
+    </form>
+</x-admin-layout>
---
+++ resources/views/admin/roles/index.blade.php
@@ -0,0 +1,42 @@
+<!-- This example requires Tailwind CSS v2.0+ -->
+<x-admin-layout>
+    <div class="flex flex-col py-20 w-full px-8">
+        <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div class="flex justify-end mb-4">
+                <a href='{{ route('admin.role.create') }}' class="px-6 py-3 text-left text-xs font-medium uppercase tracking-wider bg-green-500 text-white rounded-full mt-2 my-2">
+                    Create Role
+                </a>
+            </div>
+                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+
+                    <table class="min-w-full divide-y divide-gray-200">
+                        <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col"
+                                    class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                                    Role</th>
+                                <th scope="col" class="relative px-6 py-3">
+                                    <span class="sr-only">Edit</span>
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($roles as $role)
+                            <tr class="bg-gray-50">
+                                <td class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">
+                                    {{ $role->name }}
+                                </td>
+                                <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium justify-end space-x-8">
+                                    <a href="#" class="text-blue-400 hover:text-blue-800">Edit</a>
+                                    <a href="#" class="text-red-400 hover:text-red-800">Delete</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-admin-layout>
